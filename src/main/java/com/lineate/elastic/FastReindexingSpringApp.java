fd172b7ac985c3f5eb4e743754141857e3d9d5d3
Dmitry Borisenko <dmitry_borisenko@lineate.com>
2020-10-12 15:12:54 +0600

    IndexManagementApp Add: scheduled method that removes old indices for completed reindexing tasks.

--- src/main/java/com/lineate/elastic/FastReindexingSpringApp.java
+++ src/main/java/com/lineate/elastic/FastReindexingSpringApp.java
@@ -2,8 +2,10 @@ package com.lineate.elastic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FastReindexingSpringApp {
     public static void main(String[] args) {
         SpringApplication.run(FastReindexingSpringApp.class, args);
